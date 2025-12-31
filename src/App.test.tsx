be2664940927fa2d643f4f07a765647aeb7dd9b6
RichardBacon <richardjohnbacon@gmail.com>
2025-12-31 10:54:47 +0000

    chore: remove initial project setup files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 describe('App', () => {
   it('should render the app', () => {
     render(<App />);
-    expect(screen.getByText('Vite + React')).toBeInTheDocument();
+    expect(screen.getByText('Multi-step Form')).toBeInTheDocument();
   });
 });
