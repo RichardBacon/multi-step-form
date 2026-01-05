0949e6edcd69643b84f79ecf056322bae11f40fd
RichardBacon <richardjohnbacon@gmail.com>
2026-01-05 17:40:55 +0000

    feat: implement step progress component

--- src/components/MultiStepForm/MultiStepForm.tsx
+++ src/components/MultiStepForm/MultiStepForm.tsx
@@ -1,7 +1,12 @@
+import StepProgress from '../StepProgress/StepProgress';
 import styles from './MultiStepForm.module.css';
 
 const MultiStepForm = () => {
-  return <div className={styles.root}></div>;
+  return (
+    <div className={styles.root}>
+      <StepProgress />
+    </div>
+  );
 };
 
 export default MultiStepForm;
---
+++ src/components/StepProgress/StepProgress.tsx
@@ -0,0 +1,7 @@
+import styles from './StepProgress.module.css';
+
+const StepProgress = () => {
+  return <div className={styles.root}></div>;
+};
+
+export default StepProgress;
