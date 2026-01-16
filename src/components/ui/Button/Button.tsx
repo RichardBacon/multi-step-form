f631e0f660fff555aeffd4c9fea8959668de4f25
RichardBacon <richardjohnbacon@gmail.com>
2026-01-16 19:31:16 +0000

    feat: enhance Button component to support variant styles

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,11 +1,15 @@
+import { clsx } from 'clsx';
 import type { ReactNode } from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps {
   children: ReactNode;
+  variant?: 'primary' | 'secondary';
 }
-const Button = ({ children }: ButtonProps) => {
-  return <button className={styles.root}>{children}</button>;
+const Button = ({ children, variant = 'primary' }: ButtonProps) => {
+  return (
+    <button className={clsx(styles.root, styles[variant])}>{children}</button>
+  );
 };
 
 export default Button;
