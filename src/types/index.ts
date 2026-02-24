81d8a107ac745f691e543cc7af9428d37f797e4e
RichardBacon <richardjohnbacon@gmail.com>
2026-02-24 15:06:44 +0000

    feat: add shared types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,34 @@ export interface PersonalInfo {
   email: string;
   phone: string;
 }
+
+export type PlanId = 'arcade' | 'advanced' | 'pro';
+
+export type AddOnId =
+  | 'online-service'
+  | 'larger-storage'
+  | 'customizable-profile';
+
+export type BillingCycle = 'monthly' | 'yearly';
+
+export type Plan = {
+  id: PlanId;
+  name: string;
+  monthlyPrice: number;
+  yearlyPrice: number;
+};
+
+export type AddOn = {
+  id: AddOnId;
+  name: string;
+  description: string;
+  monthlyPrice: number;
+  yearlyPrice: number;
+};
+
+export type FormSubmission = {
+  personalInfo: PersonalInfo;
+  planId: PlanId;
+  billingCycle: BillingCycle;
+  addOnIds: AddOnId[];
+};
