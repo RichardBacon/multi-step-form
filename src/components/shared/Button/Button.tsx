87cbc7f577aa64d213059a2803faecbbf99dac09
RichardBacon <richardjohnbacon@gmail.com>
2026-02-27 09:05:49 +0000

    feat: add disabled prop to Button component

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -6,13 +6,20 @@ interface ButtonProps {
   children: ReactNode;
   variant?: 'primary' | 'secondary' | 'tertiary';
   onClick?: () => void;
+  disabled?: boolean;
 }
-const Button = ({ children, variant = 'primary', onClick }: ButtonProps) => {
+const Button = ({
+  children,
+  variant = 'primary',
+  onClick,
+  disabled = false,
+}: ButtonProps) => {
   return (
     <button
       type="button"
       className={clsx(styles.root, styles[variant])}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
